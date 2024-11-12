950c93da178f57dd2eefa32371e488449d4bc6dd
petergoodwin28 <petergoodwin28@gmail.com>
2024-11-12 13:50:58 -0500

    changed date-fns version and updated wepack config in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack(config) {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      'date-fns/locale': require.resolve('date-fns/locale'), // Resolving locale explicitly
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
