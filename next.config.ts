b0661f061a10a3315039cfcb09a5b0a2fc7572da
Peter Goodwin <petergoodwin28@gmail.com>
2025-11-22 17:39:33 -0500

    Resolving...

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,30 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  webpack(config) {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "date-fns/locale": require.resolve("date-fns/locale"),
-    };
+// const nextConfig: NextConfig = {
+//   webpack(config) {
+//     config.resolve.alias = {
+//       ...config.resolve.alias,
+//       "date-fns/locale": require.resolve("date-fns/locale"),
+//     };
 
+//     return config;
+//   },
+// };
+/** @type {import('next').NextConfig} */
+const nextConfig: NextConfig = {
+  experimental: {
+    turbo: {
+      rules: {
+        css: false,
+      },
+    },
+  },
+  webpack: (config) => {
     return config;
   },
 };
+
+module.exports = nextConfig;
 
 module.exports = nextConfig;
 
